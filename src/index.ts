a50308c02b537f1d7726dc7345bf44c9e1adf2b0
Joel Bowman <jbowwww@gmail.com>
2024-08-17 20:29:31 +0800

    Fix getAutoCompleteDetails bug which was discarding results array.
    getAutoComplete now returns raw response body JSON, results are mapped in getAutoCompleteDetails as original comments (incorrectly) previously indicated.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import { getPlaceAutocomplete } from './maps-api'
 
 export async function getAutoCompleteDetails(address: any): Promise<any> {
-    const apiKey = process.env.TOMTOM_API_KEY;
-    // get autocomplete results
-    const res = getPlaceAutocomplete(process.env.TOMTOM_API_KEY ?? "", address)
-        .then(async (autocompleteResults) => {
-            const res: any[] | PromiseLike<any[]> = [];
-            return res;
-        });
-    // loop over and get details and map results
-    return res;
+    // our API client returns the raw results array in the response body from the maps API
+    const results = await getPlaceAutocomplete(process.env.TOMTOM_API_KEY ?? "", address);
+    // map results to a structure to suit us
+    return results.map((r: { id: any; }) => ({ placeId: r.id }));
 }
