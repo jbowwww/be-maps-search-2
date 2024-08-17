5fa1ae7bc6d5048ca23f40003ae359f5dd027d5a
Joel Bowman <jbowwww@gmail.com>
2024-08-17 08:51:13 +0800

    Initial commit from original .zip file

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { getPlaceAutocomplete } from './maps-api'
+
+export async function getAutoCompleteDetails(address: any): Promise<any> {
+    const apiKey = process.env.TOMTOM_API_KEY;
+    // get autocomplete results
+    const res = getPlaceAutocomplete(process.env.TOMTOM_API_KEY ?? "", address)
+        .then(async (autocompleteResults) => {
+            const res: any[] | PromiseLike<any[]> = [];
+            return res;
+        });
+    // loop over and get details and map results
+    return res;
+}
